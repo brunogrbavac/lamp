75872ba992e1bb38d622ba01d35722f678374b8c
Bruno Grbavac <bruno.grbavac@outlook.com>
2024-02-01 16:38:37 +0100

    fix: apache root changed
    Data root for Apache2 set to /php inside of workspace for ease of use.

--- .devcontainer/.config/config.inc.php
+++ .devcontainer/.config/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-$cfg['blowfish_secret'] = 'arbitrary_string'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '/NVKjzfV]9(%i8Wt0Seev0_}VKM6ZU6K_u0a%28#_.z%rbj)S-JquTL,.qJhh.VR';
 $i = 0;
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
